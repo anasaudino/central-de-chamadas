637be60041d069589f3107e7599091657fc72e9e
Ana Luiza Saudino <leilaanasaudino@gmail.com>
2026-02-02 01:17:04 -0300

    Init | Config Django

---
+++ helpdesk/urls.py
@@ -0,0 +1,12 @@
+﻿from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
+from tickets.forms import LoginForm
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/login/', auth_views.LoginView.as_view(authentication_form=LoginForm, template_name='registration/login.html'), name='login'),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('tickets.urls')),
+]
