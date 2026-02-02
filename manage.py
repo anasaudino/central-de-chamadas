c482dae353ea9be4bf7b11cd275b4bc3a9b94929
Ana Luiza Saudino <leilaanasaudino@gmail.com>
2026-02-02 01:15:07 -0300

    Init | Estrutura inicial do projeto

---
+++ manage.py
@@ -0,0 +1,12 @@
+﻿from django.core.management import execute_from_command_line
+import os
+import sys
+
+
+def main() -> None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helpdesk.settings')
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
